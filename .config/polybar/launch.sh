e6c210ad6f50f5a2164a4a6820fa78ed195cef52
Milan Vodák <milanvodak02@gmail.com>
2023-03-13 09:29:07 +0100

    feat(polybar): load host-specific config in the launch script
    Avoid having to use multiple branches.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,6 @@
 killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar top 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -c ~/.config/polybar/$(hostname).ini top 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
