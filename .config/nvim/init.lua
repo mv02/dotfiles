be7441b7557ae162286e275488643e5dba42c2ba
Milan Vodák <milanvodak02@gmail.com>
2023-04-01 13:40:21 +0200

    feat(nvim): add keymap to toggle tab size

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,6 +19,14 @@ vim.keymap.set('n', '<C-s>', '<cmd>w<CR>')
 vim.keymap.set('n', '<leader>qq', '<cmd>qa<CR>')
 vim.keymap.set('n', '<leader>tw', function() o.wrap = not o.wrap end )
 vim.keymap.set('n', '<leader>tl', function() o.relativenumber = not o.relativenumber end )
+vim.keymap.set('n', '<leader>ts', function()
+  if o.tabstop == 4 then
+    o.tabstop = 2
+  else
+    o.tabstop = 4
+  end
+  o.shiftwidth = o.tabstop
+end)
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
