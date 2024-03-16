3e2c3aa02c1912dc93615df18b044f796c2b705d
Milan Vodák <milanvodak02@gmail.com>
2024-03-16 15:21:23 +0100

    feat(nvim): add LSP keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,3 +18,12 @@ vim.keymap.set('n', '<leader>ts', function()
   end
   o.shiftwidth = o.tabstop
 end)
+
+-- LSP
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename)
