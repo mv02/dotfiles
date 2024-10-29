93b3a54736f48848bb33e09513138c486b20166b
Milan Vodák <milanvodak02@gmail.com>
2024-10-29 12:20:39 +0100

    feat(nvim): add markdown-preview.nvim

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -43,6 +43,13 @@ return {
     },
 
     {
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        build = "cd app && yarn install",
+        init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    },
+
+    {
         "lervag/vimtex",
         ft = "tex",
         config = function()
