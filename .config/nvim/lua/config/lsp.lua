2c021f87d2cc023caa1f9a49ec1be2720070dcca
Milan Vodák <milanvodak02@gmail.com>
2026-01-13 15:13:45 +0100

    refactor(nvim): rewrite LSP config to use vim.lsp.config

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,26 +1,18 @@
-local mason = require("mason")
-local mason_lsp = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
+require("mason").setup()
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.lsp.config("*", { capabilities = capabilities })
 
-mason.setup()
-mason_lsp.setup({
-    ensure_installed = {
-        "clangd",
-        "jdtls",
-        "pyright",
-        "texlab",
-    },
-})
+local servers = {
+    "clangd",
+    "jdtls",
+    "pyright",
+    "texlab",
+}
 
-mason_lsp.setup_handlers({
-    function(server_name)
-        lspconfig[server_name].setup({
-            capabilities = capabilities,
-        })
-    end,
-})
+for _, server in ipairs(servers) do
+    vim.lsp.enable(server)
+end
 
 local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 
