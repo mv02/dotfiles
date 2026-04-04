7785cedf823394b66047d538242671b6cc8325cc
Milan Vodák <milanvodak02@gmail.com>
2026-04-04 16:59:51 +0200

    feat(lazygit): handle theme setup using themes script

--- scripts/themes.sh
+++ scripts/themes.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# kitty
 kitten themes Catppuccin-Mocha
+
+# tmux
 git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/catppuccin
+
+# lazygit
+repo_path=/tmp/catppuccin-lazygit
+config_file=~/.config/lazygit/config.yml
+git clone https://github.com/catppuccin/lazygit.git $repo_path
+echo "gui:" > $config_file
+cat $repo_path/themes/mocha/mauve.yml | sed 's/^/  /' | head -n -3 >> $config_file
