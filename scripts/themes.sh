82fcae2916be73ad0de52976105192f239b3bd6e
Milan Vodák <milanvodak02@gmail.com>
2026-04-04 17:03:47 +0200

    feat(themes): pull already existing theme repos instead of cloning
    Add a helper that prevents git clone fatal error when cloning into a
    non-empty directory and pulls newest changes instead.

--- scripts/themes.sh
+++ scripts/themes.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+function clone_or_pull() {
+    if [[ -d $2/.git ]]; then
+        git --git-dir "$2"/.git pull
+    else
+        git clone "$1" "$2"
+    fi
+}
+
 # kitty
 kitten themes Catppuccin-Mocha
 
 # tmux
-git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/catppuccin
+clone_or_pull https://github.com/catppuccin/tmux.git ~/.config/tmux/catppuccin
 
 # lazygit
 repo_path=/tmp/catppuccin-lazygit
 config_file=~/.config/lazygit/config.yml
-git clone https://github.com/catppuccin/lazygit.git $repo_path
+clone_or_pull https://github.com/catppuccin/lazygit.git $repo_path
 echo "gui:" > $config_file
 cat $repo_path/themes/mocha/mauve.yml | sed 's/^/  /' | head -n -3 >> $config_file
