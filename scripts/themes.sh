aafb3409e3d85047b49406718a8ab06464a9f9be
Milan Vodák <milanvodak02@gmail.com>
2026-04-04 17:44:03 +0200

    feat(themes): allow setting catppuccin flavor

--- scripts/themes.sh
+++ scripts/themes.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $# -lt 1 ]]; then
+    flavor=mauve
+else
+    flavor=$1
+fi
+
 function clone_or_pull() {
     if [[ -d $2/.git ]]; then
         git --git-dir "$2"/.git pull
@@ -19,9 +25,9 @@ repo_path=/tmp/catppuccin-lazygit
 config_file=~/.config/lazygit/config.yml
 clone_or_pull https://github.com/catppuccin/lazygit.git $repo_path
 echo "gui:" > $config_file
-cat $repo_path/themes/mocha/mauve.yml | sed 's/^/  /' | head -n -3 >> $config_file
+cat "$repo_path/themes/mocha/$flavor.yml" | sed 's/^/  /' | head -n -3 >> $config_file
 
 # yazi
 config_dir=~/.config/yazi
-curl -o $config_dir/theme.toml https://raw.githubusercontent.com/catppuccin/yazi/refs/heads/main/themes/mocha/catppuccin-mocha-mauve.toml
+curl -o $config_dir/theme.toml "https://raw.githubusercontent.com/catppuccin/yazi/refs/heads/main/themes/mocha/catppuccin-mocha-$flavor.toml"
 curl -o $config_dir/Catppuccin-mocha.tmTheme https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Mocha.tmTheme
