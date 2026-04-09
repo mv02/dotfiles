388f7a62d50089668c150d797f7322292ecbe949
Milan Vodák <milanvodak02@gmail.com>
2026-04-09 16:33:54 +0200

    refactor(nvim): migrate treesitter to main branch and native API
    Migrate from master to the new main branch of nvim-treesitter. Use the
    native vim.treesitter API to set up highlighting. Update telescope.nvim
    to fix errors with the new nvim-treesitter version.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,38 +2,41 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "cpp",
-                    "diff",
-                    "fish",
-                    "gitcommit",
-                    "gitignore",
-                    "html",
-                    "javascript",
-                    "json",
-                    "latex",
-                    "lua",
-                    "make",
-                    "markdown",
-                    "python",
-                    "sql",
-                    "svelte",
-                    "typescript",
-                },
 
-                highlight = {
-                    enable = true,
-                    disable = { "latex", "htmldjango" },
-                },
+        init = function()
+            -- Install parsers
+            require("nvim-treesitter").install({
+                "bash",
+                "c",
+                "cpp",
+                "diff",
+                "fish",
+                "gitcommit",
+                "gitignore",
+                "html",
+                "javascript",
+                "json",
+                "latex",
+                "lua",
+                "make",
+                "markdown",
+                "python",
+                "sql",
+                "svelte",
+                "typescript",
+            })
 
-                indent = {
-                    enable = true,
-                },
+            -- Start treesitter automatically in buffers
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local ft = vim.bo[args.buf].filetype
+                    local lang = vim.treesitter.language.get_lang(ft)
+
+                    -- Start if a parser for the filetype exists
+                    if vim.treesitter.language.add(lang) then
+                        vim.treesitter.start()
+                    end
+                end,
             })
         end,
     },
